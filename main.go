b6b2cc013fde7432aa76d156dce376e29e68134d
Mike Harris <mikeharris22@gmail.com>
2024-04-04 18:00:50 -0400

    First commit

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	p "path"
+	"strings"
+	"time"
+)
+
+func main() {
+	currentTime := time.Now()
+	userHomeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal("Unable to determine user's home directory: ", err)
+	}
+	targetFolder := fmt.Sprintf("%d-%02d-%02d-%s", currentTime.Year(), currentTime.Month(), currentTime.Day(), "Archive")
+	appFolder := "DeskClean"
+	cleanFolder := "Desktop"
+	targetRoot := p.Join(userHomeDir, appFolder, targetFolder)
+	sourceRoot := p.Join(userHomeDir, cleanFolder)
+	sourceFs := os.DirFS(sourceRoot)
+	err = createTargetDirectory(userHomeDir, appFolder, targetFolder)
+	if err != nil {
+		log.Fatal("Unable to create target directory. ", err)
+	}
+
+	fs.WalkDir(sourceFs, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			log.Fatal(err)
+		}
+		if d.Type().IsRegular() || d.Type().IsDir() {
+			if strings.HasPrefix(d.Name(), ".") {
+				fmt.Println("Skipping hidden file", path)
+			} else {
+				err = os.Rename(p.Join(sourceRoot, path), p.Join(targetRoot, path))
+				if err != nil {
+					fmt.Println("Failed to move file - ", err)
+				}
+			}
+		}
+		return nil
+	})
+}
+
+func createTargetDirectory(userHomeDir, appFolder, targetFolder string) error {
+	appPath := p.Join(userHomeDir, appFolder)
+	targetPath := p.Join(appPath, targetFolder)
+	if _, err := os.Stat(appPath); errors.Is(err, os.ErrNotExist) {
+		err := os.Mkdir(appPath, os.ModePerm)
+		if err != nil {
+			return err
+		}
+	}
+	if _, err := os.Stat(targetPath); errors.Is(err, os.ErrNotExist) {
+		err := os.Mkdir(targetPath, os.ModePerm)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
