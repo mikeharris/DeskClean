8117c68570fc39216d6ffeffcce246c6326bf705
Mike Harris <mikeharris22@gmail.com>
2024-05-30 14:47:24 -0400

    Added config struct and refactored a bit to run from GUI.

--- main.go
+++ main.go
@@ -9,30 +9,69 @@ import (
 	p "path"
 	"strings"
 	"time"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/driver/desktop"
+	"fyne.io/fyne/v2/widget"
 )
 
+type AppConfig struct {
+	AppName      string
+	HomeDir      string
+	AppFolder    string
+	SourcePath   string
+	TargetPath   string
+	TargetFolder string
+}
+
 func main() {
+	ac := AppConfig{}
+
+	ac.AppName = "DeskClean"
 	currentTime := time.Now()
 	userHomeDir, err := os.UserHomeDir()
 	if err != nil {
 		log.Fatal("Unable to determine user's home directory: ", err)
 	}
-	targetFolder := fmt.Sprintf("%d-%02d-%02d-%s", currentTime.Year(), currentTime.Month(), currentTime.Day(), "Archive")
-	appFolder := "DeskClean"
+	ac.HomeDir = userHomeDir
+	targetFolderLabel := "Archive"
+	ac.TargetFolder = fmt.Sprintf("%d-%02d-%02d-%s", currentTime.Year(), currentTime.Month(), currentTime.Day(), targetFolderLabel)
+	ac.AppFolder = ac.AppName
 	cleanFolder := "Desktop"
-	targetRoot := p.Join(userHomeDir, appFolder, targetFolder)
-	sourceRoot := p.Join(userHomeDir, cleanFolder)
-	err = createTargetDirectory(userHomeDir, appFolder, targetFolder)
+	ac.TargetPath = p.Join(ac.HomeDir, ac.AppFolder, ac.TargetFolder)
+	ac.SourcePath = p.Join(userHomeDir, cleanFolder)
+
+	a := app.New()
+	w := a.NewWindow(ac.AppName)
+
+	if desk, ok := a.(desktop.App); ok {
+		m := fyne.NewMenu(ac.AppName,
+			fyne.NewMenuItem("Run Now", func() {
+				runSweep(ac)
+			}))
+		desk.SetSystemTrayMenu(m)
+	}
+
+	w.SetContent(widget.NewLabel("Fyne System Tray"))
+	w.SetCloseIntercept(func() {
+		w.Hide()
+	})
+	a.Run()
+}
+
+func runSweep(ac AppConfig) {
+	err := createTargetDirectory(ac.TargetPath)
 	if err != nil {
 		log.Fatal("Unable to create target directory. ", err)
 	}
-	err = moveSourceFiles(sourceRoot, targetRoot)
+	err = moveFiles(ac.SourcePath, ac.TargetPath)
 	if err != nil {
 		log.Fatal("Failed to move source files. ", err)
 	}
 }
 
-func moveSourceFiles(sourceRoot, targetRoot string) error {
+func moveFiles(sourceRoot, targetRoot string) error {
 	sourceFs := os.DirFS(sourceRoot)
 	fs.WalkDir(sourceFs, ".", func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
@@ -53,9 +92,8 @@ func moveSourceFiles(sourceRoot, targetRoot string) error {
 	return nil
 }
 
-func createTargetDirectory(userHomeDir, appFolder, targetFolder string) error {
-	appPath := p.Join(userHomeDir, appFolder)
-	targetPath := p.Join(appPath, targetFolder)
+func createTargetDirectory(targetPath string) error {
+	appPath := p.Dir(targetPath)
 	if _, err := os.Stat(appPath); errors.Is(err, os.ErrNotExist) {
 		err := os.Mkdir(appPath, os.ModePerm)
 		if err != nil {
