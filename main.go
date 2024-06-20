5f134cdf92a89527348ba005cbd38efebf71006c
Mike Harris <mikeharris22@gmail.com>
2024-06-19 22:33:20 -0400

    Added slog to file capability.  Refactored some methods.  Cleaned up icons.

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import (
 	"errors"
 	"fmt"
 	"io/fs"
-	"log"
+	"log/slog"
 	"os"
-	p "path"
+	"path"
 	"runtime"
 	"strings"
 	"time"
@@ -24,10 +24,11 @@ import (
 
 const (
 	appNamespace      string = "com.github.mikeharris.DeskClean"
-	macAppExt         string = ".app"
 	sweptMenuLabel    string = "Swept at %s"
 	sweepMenuLabel    string = "Sweep now"
 	settingsMenuLabel string = "Settings"
+	logFileExt        string = ".log"
+	appNameDefault    string = "DeskClean"
 )
 
 var (
@@ -45,6 +46,12 @@ func main() {
 
 	a := app.NewWithID(appNamespace)
 	prefs := a.Preferences()
+	appName := prefs.StringWithFallback("AppName", appNameDefault)
+
+	logger := slog.New(slog.NewJSONHandler(getLogFile(runtime.GOOS, appName, appName+logFileExt), nil))
+	slog.SetDefault(logger)
+	slog.SetLogLoggerLevel(slog.LevelDebug)
+
 	var menu *fyne.Menu
 	lastSweepMenu := fyne.NewMenuItem(fmt.Sprintf(sweptMenuLabel, prefs.String("LastSweep")), func() {})
 
@@ -52,32 +59,25 @@ func main() {
 		initAppDefaults(a.Preferences())
 	}
 
-	var e string
-	if runtime.GOOS == "darwin" {
-		e = p.Join(xdg.ApplicationDirs[0], prefs.String("AppName")) + macAppExt
-	} else {
-		var err error
-		e, err = os.Executable()
-		if err != nil {
-			log.Fatal("Unable to determine executable. ", err)
-		}
+	exe, err := getAppExecutable(runtime.GOOS, appName)
+	if err != nil {
+		panic("Unable to determine app executable.")
 	}
-
 	startApp := &autostart.App{
 		Name:        appNamespace,
-		DisplayName: prefs.String("AppName"),
-		Exec:        []string{e},
+		DisplayName: appName,
+		Exec:        []string{exe},
 	}
 	autoLaunch := prefs.Bool("AutoLaunchApp")
 
-	w := a.NewWindow(prefs.String("AppName") + " Settings")
+	w := a.NewWindow(appName + " Settings")
 
 	if desk, ok := a.(desktop.App); ok {
-		menu = fyne.NewMenu(prefs.String("AppName"),
+		menu = fyne.NewMenu(appName,
 			fyne.NewMenuItem(sweepMenuLabel, func() {
-				err := sweepFiles(prefs.String("SourcePath"), getTargetPath(prefs))
+				err := sweepFiles(os.DirFS(prefs.String("SourcePath")), prefs.String("SourcePath"), getTargetPath(prefs))
 				if err != nil {
-					log.Println("Failed to move source files. ", err)
+					slog.Warn("Failed to move source files. ", slog.Any("error", err))
 				}
 				prefs.SetString("LastSweep", time.Now().Format(time.Kitchen))
 				lastSweepMenu.Label = fmt.Sprintf(sweptMenuLabel, prefs.String("LastSweep"))
@@ -102,30 +102,37 @@ func main() {
 			autoLaunch = !autoLaunch
 			if startApp.IsEnabled() && !autoLaunch {
 				if err := startApp.Disable(); err != nil {
-					log.Fatal(err)
+					slog.Warn("Unable to unset auto launch.", slog.Any("error", err))
 				}
 			} else if !startApp.IsEnabled() && autoLaunch {
-				log.Println("Creating LaunchAgent plist.")
+				slog.Info("Creating LaunchAgent plist.")
 				if err := startApp.Enable(); err != nil {
-					log.Fatal(err)
+					slog.Warn("Unable to set auto launch.", slog.Any("error", err))
 				}
 			}
 		}
 		w.Hide()
 	})
 
+	// Invoke background ticker with reasonable tick rate
 	sweepTicker := time.NewTicker(60 * time.Minute)
+
+	// Reset ticker if preference is set to anything other than on demand
 	if prefs.Int("RunIntervalMinutes") > -1 {
 		sweepTicker.Reset(time.Duration(prefs.Int("RunIntervalMinutes")) * time.Minute)
 	}
 
+	// Create a data binding with the pref RunIntervalMinutes
 	runInterval := binding.BindPreferenceInt("RunIntervalMinutes", prefs)
+
+	// Add change listener to the RunIntervalMinutes property/preference
 	callback := binding.NewDataListener(func() {
 		ri, _ := runInterval.Get()
 		resetChan <- ri
 	})
 	runInterval.AddListener(callback)
 
+	// Start background sweeper thread
 	go func() {
 		for {
 			select {
@@ -134,15 +141,15 @@ func main() {
 			case i := <-resetChan:
 				if i > -1 {
 					sweepTicker.Reset(time.Duration(i) * time.Minute)
-					log.Printf("Reset timer to %d minutes.\n", prefs.Int("RunIntervalMinutes"))
+					slog.Info("Set sweep timer event.", slog.Int("RunIntervalMinutes", prefs.Int("RunIntervalMinutes")))
 				} else {
-					log.Println("Sweeper set to run on demand.")
+					slog.Info("Sweeper set to run on demand.")
 				}
 			case <-sweepTicker.C:
 				if prefs.Int("RunIntervalMinutes") > 0 {
-					err := sweepFiles(prefs.String("SourcePath"), getTargetPath(prefs))
+					err := sweepFiles(os.DirFS(prefs.String("SourcePath")), prefs.String("SourcePath"), getTargetPath(prefs))
 					if err != nil {
-						log.Println("Failed to move source files. ", err)
+						slog.Error("Failed to sweep source files.", slog.Any("error", err))
 					}
 					prefs.SetString("LastSweep", time.Now().Format(time.Kitchen))
 					lastSweepMenu.Label = fmt.Sprintf(sweptMenuLabel, prefs.String("LastSweep"))
@@ -156,6 +163,48 @@ func main() {
 	doneChan <- true
 }
 
+func getLogFile(osName, appName, logFilename string) *os.File {
+	var log string
+
+	switch osName {
+	case "darwin":
+		log = path.Join(xdg.Home, "Library", "Logs", appName, logFilename)
+	case "default":
+		log = path.Join(xdg.DataHome, appName, "logs", logFilename)
+
+	}
+
+	if _, err := os.Stat(log); os.IsNotExist(err) {
+		os.MkdirAll(path.Dir(log), 0700)
+	}
+
+	logFile, err := os.OpenFile(log, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		slog.Error("Unable to create log file.", "file", log)
+		logFile = os.Stdout
+	}
+
+	return logFile
+}
+
+func getAppExecutable(osName, appName string) (string, error) {
+	var e string
+	var err error
+
+	switch osName {
+	case "darwin":
+		e = path.Join(xdg.ApplicationDirs[0], appName) + ".app"
+	case "default":
+		e, err = os.Executable()
+		if err != nil {
+			return "", err
+		}
+
+	}
+
+	return e, nil
+}
+
 func makeSettingsUI(pref fyne.Preferences) fyne.CanvasObject {
 	al := widget.NewLabel(pref.String("HomeDir"))
 	ri := widget.NewSelect(allowedRunIntervals, func(value string) { pref.SetString("RunInterval", value) })
@@ -166,7 +215,7 @@ func makeSettingsUI(pref fyne.Preferences) fyne.CanvasObject {
 
 	af := widget.NewEntryWithData(binding.BindPreferenceString("AppFolder", pref))
 	af.OnChanged = func(s string) {
-		al.SetText(p.Join(p.Join(pref.String("HomeDir"), s)))
+		al.SetText(path.Join(pref.String("HomeDir"), s))
 	}
 
 	wc := container.NewPadded(container.NewPadded(container.New(layout.NewFormLayout(),
@@ -182,8 +231,8 @@ func makeSettingsUI(pref fyne.Preferences) fyne.CanvasObject {
 }
 
 func initAppDefaults(pref fyne.Preferences) {
-	pref.SetString("AppName", "DeskClean")
-	pref.SetString("AppFolder", "DeskClean")
+	pref.SetString("AppName", appNameDefault)
+	pref.SetString("AppFolder", appNameDefault)
 	pref.SetString("HomeDir", xdg.Home)
 	pref.SetString("TargetFolderLabel", "Archive")
 	pref.SetString("TargetFolderSeperator", "-")
@@ -193,23 +242,23 @@ func initAppDefaults(pref fyne.Preferences) {
 	pref.SetString("SourcePath", xdg.UserDirs.Desktop)
 	pref.SetBool("FirstRun", false)
 	pref.SetBool("AutoLaunchApp", false)
-	log.Println("Configuration initialized.")
+	slog.Info("Configuration initialized to defaults.")
 }
 
 func getTargetPath(pref fyne.Preferences) string {
 	now := time.Now()
 	dateStr := now.Format(pref.String("TargetFolderDateScheme"))
 	folderDateLabel := fmt.Sprintf("%s%s%s", dateStr, pref.String("TargetFolderSeperator"), pref.String("TargetFolderLabel"))
-	return p.Join(pref.String("HomeDir"), pref.String("AppFolder"), folderDateLabel)
+	return path.Join(pref.String("HomeDir"), pref.String("AppFolder"), folderDateLabel)
 }
 
-func sweepFiles(sourcePath, targetPath string) error {
+func sweepFiles(fsys fs.FS, sourcePath, targetPath string) error {
 	moveCount := 0
 	skippedCount := 0
-	sourceFs := os.DirFS(sourcePath)
+	errorCount := 0
 	targeExists := false
 
-	fs.WalkDir(sourceFs, ".", func(path string, d fs.DirEntry, err error) error {
+	fs.WalkDir(fsys, ".", func(p string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
@@ -221,16 +270,17 @@ func sweepFiles(sourcePath, targetPath string) error {
 					// Determine if parent path needs created and only create if there is a file/folder to write
 					err = createTargetDirectory(targetPath)
 					if err != nil {
-						log.Println("Unable to create target directory. ", err)
+						slog.Error("Unable to create target directory. ", slog.Any("error", err))
 						// If we cannot create the containing folder then fail fast
 						return err
 					}
 					targeExists = true
 				}
 
-				err = os.Rename(p.Join(sourcePath, path), p.Join(targetPath, path))
+				err = os.Rename(path.Join(sourcePath, p), path.Join(targetPath, p))
 				if err != nil {
-					log.Println("Failed to move file - ", err)
+					errorCount++
+					slog.Warn("Failed to move file.", slog.Any("error", err), slog.String("file", p))
 				} else {
 					moveCount++
 				}
@@ -238,21 +288,13 @@ func sweepFiles(sourcePath, targetPath string) error {
 		}
 		return nil
 	})
-	log.Printf("Skipped %d hidden files.\n", skippedCount)
-	log.Printf("Cleaned %d files.\n", moveCount)
+	slog.Info("Sweep completed.", slog.Int("sweptFileCount", moveCount), slog.Int("skippedFileCount", skippedCount), slog.Int("fileErrorCount", errorCount))
 	return nil
 }
 
 func createTargetDirectory(targetPath string) error {
-	appPath := p.Dir(targetPath)
-	if _, err := os.Stat(appPath); errors.Is(err, os.ErrNotExist) {
-		err := os.Mkdir(appPath, os.ModePerm)
-		if err != nil {
-			return err
-		}
-	}
 	if _, err := os.Stat(targetPath); errors.Is(err, os.ErrNotExist) {
-		err := os.Mkdir(targetPath, os.ModePerm)
+		err := os.MkdirAll(targetPath, os.ModePerm)
 		if err != nil {
 			return err
 		}
@@ -276,8 +318,7 @@ func runIntervalToInt(text string) int {
 		return 240
 	case "every 12 hours":
 		return 720
-	case "every 24 hours":
-		return 1440
+
 	default:
 		return -1
 	}
